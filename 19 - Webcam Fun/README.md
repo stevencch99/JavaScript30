48b153e810f1293128ba594d2484781bfa8b4a79
Steven C <stevencch99@gmail.com>
2020-12-20 15:03:57 +0800

    Update reading progress of day 19

--- 19 - Webcam Fun/README.md
+++ 19 - Webcam Fun/README.md
@@ -6,4 +6,18 @@
 - To fix vulnerabilities if needed: `npm audit fix`
 - Start a server: `npm start`
 
+- To return the `setInterval` so we can have access to the interval and call "clearInterval" on it.
+
+### To reset Camera in macOS
+
+The answer to the prompt is persistent and the app will be listed in
+**System Preferences > Security & Privacy > Privacy > Camera**.
+
+macOS will only prompt you once for each app. It will not prompt you again about apps in the list. If you want to change the permission for these apps, you will have to do it from the list above.
+
+Reset by command: `tccutil reset Camera`
+
+> Ref: <https://blog.addpipe.com/camera-and-microphone-access-in-macos-mojave/>
+
 ## Additional Information
+
--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -8,5 +8,44 @@ function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
       console.log(localMediaStream);
+      // video.src = window.URL.createObjectURL(localMediaStream);
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch(err => {
+      console.error(`You denied the webcam, maybe you should not do that.`, err);
     })
 }
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height  = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height
+ 
+  // return so we can have access to the interval and call "clearInterval" on it.
+  return setInterval(() => {
+    // draw image from the location 0, 0 to video width, video height in every 16 milliseconds.
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto() {
+  // played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  // console.log(data);
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.textContent = 'Download Image';
+  strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+
+video.addEventListener('canplay', paintToCanvas);
