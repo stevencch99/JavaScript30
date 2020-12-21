5aabaabc68d1ba1646d1e047cd2f6dfd1f4c13f1
Steven C <stevencch99@gmail.com>
2020-12-21 17:27:06 +0800

    Update reading progress of Day 19

--- 19 - Webcam Fun/README.md
+++ 19 - Webcam Fun/README.md
@@ -2,11 +2,25 @@
 
 ## Key points
 
-- Install dependances: `npm install`
+Use `browser-sync` package to serve the page due to security concern.
+- Install dependances in package.json: `npm install`
 - To fix vulnerabilities if needed: `npm audit fix`
 - Start a server: `npm start`
-
+- Visit the webpage through localhost:3000
 - To return the `setInterval` so we can have access to the interval and call "clearInterval" on it.
+
+- Add effects by play with the pixels (loop through image data from canvas content).
+
+  ```js
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] // Red
+    pixels.data[i + 1] // Green
+    pixels.data[i + 2] // Blue
+    // omit
+  }
+  ```
+
+## Additional Information
 
 ### To reset Camera in macOS
 
@@ -18,6 +32,4 @@ macOS will only prompt you once for each app. It will not prompt you again about
 Reset by command: `tccutil reset Camera`
 
 > Ref: <https://blog.addpipe.com/camera-and-microphone-access-in-macos-mojave/>
-
-## Additional Information
 
--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -7,8 +7,6 @@ const snap = document.querySelector('.snap');
 function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
-      console.log(localMediaStream);
-      // video.src = window.URL.createObjectURL(localMediaStream);
       video.srcObject = localMediaStream;
       video.play();
     })
@@ -19,14 +17,27 @@ function getVideo() {
 
 function paintToCanvas() {
   const width = video.videoWidth;
-  const height  = video.videoHeight;
+  const height = video.videoHeight;
   canvas.width = width;
   canvas.height = height
- 
+
   // return so we can have access to the interval and call "clearInterval" on it.
   return setInterval(() => {
     // draw image from the location 0, 0 to video width, video height in every 16 milliseconds.
     ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    // mess with them
+    // pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    pixels = greenScreen(pixels);
+
+    // toggle delayed frame effect
+    // ctx.globalAlpha = 0.5;
+
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -37,15 +48,58 @@ function takePhoto() {
 
   // take the data out of the canvas
   const data = canvas.toDataURL('image/jpeg');
-  // console.log(data);
   const link = document.createElement('a');
   link.href = data;
   link.setAttribute('download', 'handsome');
   link.textContent = 'Download Image';
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
   strip.insertBefore(link, strip.firstChild);
 }
 
-getVideo();
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 40; // Red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // Green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
+}
 
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // Red
+    pixels.data[i + 100] = pixels.data[i + 1]; // Green
+    pixels.data[i - 150] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // change the transparency to 0 to take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+  return pixels;
+}
+
+getVideo();
 
 video.addEventListener('canplay', paintToCanvas);
